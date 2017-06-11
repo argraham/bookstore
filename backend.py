4022e3e8d7fb4b02c12cb212e9fffbc294335601
argraham <graham_andrew@hotmail.co.uk>
2017-06-11 15:01:10 +0100

    Firts commit

---
+++ backend.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title,author,year,isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * from book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="", author="", year="", isbn=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * from book where title = ? or author = ? or year = ? or isbn = ?", (title,author,year,isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+
+def update(id, title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE book SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
+    conn.commit()
+    conn.close()
+
+connect()
+#insert("The Sun", "John Smith", 1918, 16456131)
+#delete(3)
+#update(2,"The moon", "John Smooth", 1917, 99)
+#print (view())
+#print (search(author = "John Smith"))
